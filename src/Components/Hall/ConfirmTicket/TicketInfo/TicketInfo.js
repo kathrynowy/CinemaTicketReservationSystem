4d4ec24305a05b5d6e68152f7a321d659e90a760
Katya <katuha_maksimova@mail.ru>
2019-03-12 22:27:32 +0300

    Revert "add initial data about selected additional services"
    This reverts commit 8f5df2e24b1b960824c9e30eb2b6d0c11401911a.

--- src/Components/Hall/ConfirmTicket/TicketInfo/TicketInfo.js
+++ src/Components/Hall/ConfirmTicket/TicketInfo/TicketInfo.js
@@ -4,31 +4,6 @@ import './TicketInfo.scss'
 
 
 class TicketInfo extends Component {
-  constructor(props) { 
-    super(props); 
-    const services = (props.additionalServices.filter(service => service.cinemaId == props.cinemaId))[0].services; 
-    this.state = { 
-      form: services.map((service, index) => { 
-        return ({ 
-          id: index, 
-          value: false, 
-          name: service.name, 
-          cost: service.cost 
-        });
-      }) 
-    } 
-  }
-  
-  handleSelect = (ticketId, serviceName, cost, index) => { 
-    const newForm = this.state.form; 
-    newForm[index].value = !newForm[index].value; 
-    this.setState({ 
-      form: this.state.form 
-    }) 
-    this.props.handleSelect(ticketId, index); 
-    alert('ticketId: ' + ticketId + ' name: ' + serviceName + ' cost: ' + cost + ' index: ' + index); 
-  } 
-
   render() {
     const { row, seat, additionalServices, movieName, ticketId, cinemaId } = this.props;
     const services = (additionalServices.filter(service => service.cinemaId == cinemaId))[0].services;
@@ -57,16 +32,10 @@ class TicketInfo extends Component {
                     id={"option" + row + seat + service.name}
                     name="option1"
                     type="checkbox"
-                    checked={this.state.form[index].value}
-                    onChange={() => { 
-                      return this.handleSelect(ticketId-1, service.name, service.cost, index); 
-                    }}
                   />
-                  <label className="input-group__name" htmlFor={"option" + row + seat + service.name}>
-                    {service.name}
-                  </label>
+                  <label className="input-group__name" htmlFor={"option" + row + seat + service.name}>{service.name}</label>
                 </div>
-              );
+              )
             })
           }
         </div>
