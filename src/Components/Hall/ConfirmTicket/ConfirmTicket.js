8e38659fb296fad14ae2ec769748579763494dc6
Katya <katuha_maksimova@mail.ru>
2019-03-12 22:27:32 +0300

    add initial data about selected additional services

--- src/Components/Hall/ConfirmTicket/ConfirmTicket.js
+++ src/Components/Hall/ConfirmTicket/ConfirmTicket.js
@@ -7,6 +7,41 @@ import './ConfirmTicket.scss'
 
 
 class ConfirmTicket extends Component {
+  state = {
+    choosedServices: [
+      {
+        ticketId: 0,
+        service: [0,2]
+      }
+    ]
+  }
+  handleSelect = (ticketId, serviceId) => {
+    const services = this.state.choosedServices;
+
+    if (services.length === 0) {
+      services.push({
+        ticketId: ticketId,
+        service: [serviceId]
+      });
+
+      this.setState({
+        choosedServices: services
+      })
+    }
+
+    services.map((service) => {
+      if (service.ticketId === ticketId) {
+        service.service.includes(serviceId) 
+          ? service.service = service.service.filter(id => id !== serviceId)
+          : service.service.push(serviceId)
+      } 
+    }) 
+    this.setState({
+      choosedServices: services
+    })
+  }
+
+
   render() {
     const { additionalServices, selectedSeats } = this.props;
     return (
@@ -25,6 +60,7 @@ class ConfirmTicket extends Component {
                 cinemaId={seat.cinemaId}
                 additionalServices={additionalServices}
                 movieName={movie.name}
+                handleSelect={this.handleSelect}
               />
             );
           })
