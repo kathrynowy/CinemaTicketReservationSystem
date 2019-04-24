2764748cff7d782d969b5e48b94ce2210c5a5e2c
katerina <katuha_maksimova@mail.ru>
2019-04-24 16:54:05 +0300

    initial configuration of seats on canvas

--- src/Components/Hall/Hall.js
+++ src/Components/Hall/Hall.js
@@ -1,16 +1,69 @@
 import React, { Component } from 'react';
 
-import SelectSeats from './SelectSeats/SelectSeats.js'
-import SeatsInfo from './SeatsInfo/SeatsInfo.js'
+import SelectSeats from './SelectSeats/SelectSeats.js';
+import SeatsInfo from './SeatsInfo/SeatsInfo.js';
+import Konva from 'konva';
+import { Stage, Layer, Star, Text, Rect, Shape, Line, g } from 'react-konva';
+
 import './Hall.scss'
 
 
 class Hall extends Component {
+  createSeats(seats) {
+    const newseats = [];
+    let rows = [];
+    seats.map((seat, index) => {
+      rows = [];
+      [...Array(seat.amountOfSeats)].map((value, i) => {
+        rows.push(
+          <div>
+            <Rect
+              x={(i + 1) * 33} y={seat.row * 33} width={30} height={30}
+              shadowBlur={10}
+              fillLinearGradientStartPoint={{ x: -40, y: -40 }}
+              fillLinearGradientEndPoint={{ x: 50, y: 50 }}
+              fillLinearGradientColorStops={[0, 'red', 1, 'yellow']}
+            />
+            <Text text="Try click on rect" />
+          </div>
+        )
+      })
+      newseats.push(...rows);
+      rows = [];
+    })
+    return newseats
+  }
+
   render() {
     const { cinemaId, movieId, hallId, time } = this.props;
     return (
       <div className="hall-container">
         <div className="hall">
+          <Stage width={200} height={200} >
+            <Layer>
+              <Text text="Try click on rect" fill="pink" />
+              {
+                this.createSeats(this.props.seats.hall || [])
+                /* this.props.seats.hall.map((seat, index) => (
+                  <Rect
+                    x={seat.seat * 33} y={seat.row * 33} width={30} height={30}
+                    shadowBlur={10}
+                    fillLinearGradientStartPoint={{ x: -40, y: -40 }}
+                    fillLinearGradientEndPoint={{ x: 50, y: 50 }}
+                    fillLinearGradientColorStops={[0, 'red', 1, 'yellow']}
+                  />
+                ))
+                : <Rect
+                  x={33} y={33} width={30} height={30}
+                  shadowBlur={10}
+                  fillLinearGradientStartPoint={{ x: -40, y: -40 }}
+                  fillLinearGradientEndPoint={{ x: 50, y: 50 }}
+                  fillLinearGradientColorStops={[0, 'red', 1, 'yellow']}
+                /> */
+              }
+
+            </Layer>
+          </Stage>
           <div className="hall__screen"></div>
           <SelectSeats
             toggleSeat={this.props.onToggleSeat}
